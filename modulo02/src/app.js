3e088337da782f801089c8075e86e68a2666e1eb
Pietro Rocha <pietrodfsan@gmail.com>
2024-06-10 17:57:53 -0300

    Finalizando modulo02

--- modulo02/src/app.js
+++ modulo02/src/app.js
@@ -1,23 +1,27 @@
-const express = require('express');
-const routes = require('./routes');
+import express from "express";
+import mongoose, { mongo } from "mongoose";
+import routes from "./routes";
 
-class App{
-
-  constructor(){
+class App {
+  constructor() {
     this.server = express();
+
+    mongoose.connect(
+      "mongodb+srv://pietrodfsan:senhorzap@cluster0.idog021.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    );
 
     this.middlewares();
     this.routes();
   }
 
-  middlewares(){
+  middlewares() {
     this.server.use(express.json());
   }
 
-  routes(){
+  routes() {
     this.server.use(routes);
   }
-
 }
 
-module.exports = new App().server;
+export default new App().server;
